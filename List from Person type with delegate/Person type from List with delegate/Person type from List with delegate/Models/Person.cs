76d7132661c6c270318e318940c69ade77669c9c
anarmh <anarmh@code.edu.az>
2022-11-17 19:38:01 +0400

    Add List from Person type

---
+++ List from Person type with delegate/Person type from List with delegate/Person type from List with delegate/Models/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person_type_from_List_with_delegate.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public double Salary { get; set; }
+        public string Address { get; set; }
+
+        public Person(string name, string surname, double salary, string address)
+        {
+            Name = name;
+            Surname = surname;
+            Salary = salary;
+            Address = address;
+        }
+
+    }
+}
---
+++ List from Person type with delegate/Person type from List with delegate/Person type from List with delegate/Program.cs
@@ -0,0 +1,39 @@
+﻿using Person_type_from_List_with_delegate.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Person_type_from_List_with_delegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Person> list = new List<Person>();
+            list.Add(new Person("Anar", "Huseynov", 800, "5 mkr"));
+            list.Add(new Person("Cavid", "Rzayev", 800, "11 mkr"));
+            list.Add(new Person("Rafael", "Babaverdiyev", 600, "26-ci mehelle"));
+            list.Add(new Person("Xeyyam", "Salmanov", 3000, "Hovsan"));
+            list.Add(new Person("Samir", "Ehmedov", 2000, "13 mkr"));
+            
+
+            ShowSalaryPersonMoreThanThousand(CheckSalaryPersonMoreThanThousand, list);
+        }
+
+
+        public static bool CheckSalaryPersonMoreThanThousand(double salary)
+        {
+            return salary > 1000;
+        }
+
+        public static void ShowSalaryPersonMoreThanThousand(Predicate<double> predicate, List<Person> list)
+        {
+            foreach (var item in list)
+            {
+                if(predicate(item.Salary))
+                    Console.WriteLine($"{item.Name} {item.Surname} {item.Salary} {item.Address}");
+            }
+           
+        }
+    }
+}
